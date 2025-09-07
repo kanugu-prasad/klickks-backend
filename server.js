ba240d7b8f4f0ceb628330f3c4ddcb9e73b856f4
kanuguprasad <mudirajprasad123@gmail.com>
2025-09-07 20:55:55 +0530

    done

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-const authRoutes = require("./routes/auth");
+const authRoutes = require("./routes/auth"); 
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api", authRoutes);
+app.use("/api", authRoutes); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
